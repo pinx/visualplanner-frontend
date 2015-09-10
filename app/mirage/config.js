9f99a0ec8394b0c83aa39010e3a649dc6b0e0447
pinx <marcel.van.pinxteren@gmail.com>
2015-09-10 10:49:56 +0200

    Move to Ember 2.0.0

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -1,8 +1,6 @@
 export default function() {
 
-  this.namespace = 'api/v1';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
+  // These comments are here to help you get started. Feel free to delete them.
   this.get('/activities');
   this.post('activities');
   this.put('activities');
@@ -12,6 +10,14 @@ export default function() {
   this.post('resources');
   this.put('resources');
   this.del('/resources/:id');
+  /*
+    Config (with defaults).
+
+    Note: these only affect routes defined *after* them!
+  */
+  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
+  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
+  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
 
   /*
     Route shorthand cheatsheet
@@ -57,7 +63,7 @@ export default function() {
   /*
     Function fallback. Manipulate data in the db via
 
-      - db.{collection} // returns all the data defined in /app/mirage/fixtures/{collection}.js
+      - db.{collection}
       - db.{collection}.find(id)
       - db.{collection}.where(query)
       - db.{collection}.update(target, attrs)
@@ -66,13 +72,10 @@ export default function() {
     // Example: return a single object with related models
     this.get('/contacts/:id', function(db, request) {
       var contactId = +request.params.id;
-      var contact = db.contacts.find(contactId);
-      var addresses = db.addresses
-        .filterBy('contact_id', contactId);
 
       return {
-        contact: contact,
-        addresses: addresses
+        contact: db.contacts.find(contactId),
+        addresses: db.addresses.where({contact_id: contactId});
       };
     });
 
