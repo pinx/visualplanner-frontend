86fd284ed6ef966978d4b35f6a9b643baf4d946c
pinx <marcel.van.pinxteren@gmail.com>
2015-09-10 15:38:20 +0200

    Ember-cli updates, ftp to host

---
+++ tests/acceptance/index-test.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'visualplanner-frontend/tests/helpers/start-app';
+
+module('Acceptance | index', {
+  beforeEach: function() {
+    this.application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(this.application, 'destroy');
+  }
+});
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
